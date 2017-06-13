69aacd275213390da744079546dbcaab94f4e920
dr4g0nbyt3 <dr4g0nbyt3@gmail.com>
2017-06-13 11:20:35 -0400

    testing git

---
+++ src/main/java/com/shoes/User.java
@@ -0,0 +1,54 @@
+package com.shoes;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mbrune on 6/10/2017.
+ */
+public class User implements Serializable {
+    private String name;
+    private String email;
+    private String password;
+    private List<Site> sites;
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        sites = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return  email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<Site> getSites() {
+        return sites;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addSite(Site site) {
+        sites.add(site);
+    }
+}
---
+++ src/main/java/com/shoes/stockChecker.java
@@ -0,0 +1,10 @@
+package com.shoes;
+
+/**
+ * Created by Mbrune on 6/10/2017.
+ */
+public class stockChecker {
+    private Scraper scraper;
+
+    //public stockChecker
+}
